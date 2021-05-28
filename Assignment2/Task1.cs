67338ae1430e9ba3e893dac071bfe8d80ae6d0b8
Hritom Roy <hroy542@UoA.auckland.ac.nz>
2021-05-28 21:31:11 +1200

    28/5

---
+++ Assignment2/Task1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class FiniteStateTable
+    {
+        //constructor
+        public FiniteStateTable() {
+            Console.WriteLine("Default constructor.");
+        }
+        
+        //Variables
+        public cell_FST[,] FST = new cell_FST[3, 3];
+
+        struct cell_FST
+        {
+            public int next_state;
+            public void (*action) (void);
+        }
+
+        //Methods
+        void SetNextState(int i, int j, int next_state)
+        {
+            FST[i, j].next_state = this.next_state;
+        }
+        void SetActions(int i, int j, int action)
+        {
+            FST[i, j].action = this.action;
+        }
+
+        int GetNextState(int i, int j)
+        {
+            return FST[i, j].next_state;
+        }
+        int GetActions(int i, int j)
+        {
+            return 0;
+        }
+
+        //Main function
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
