197bb75b4074717d35c5588dd0aca2c82936a17e
ljen819 <68795621+ljen819@users.noreply.github.com>
2021-06-01 20:48:41 +1200

    Update Task1.cs

--- Assignment2/Task1.cs
+++ Assignment2/Task1.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Assignment2
 {
@@ -12,39 +13,59 @@ namespace Assignment2
         public FiniteStateTable() {
             Console.WriteLine("Default constructor.");
         }
-        
-        //Variables
-        public cell_FST[,] FST = new cell_FST[3, 3];
+
+        public int numStates;
+        public int numEvents;
+
+        public FiniteStateTable(int numStates, int numEvents)
+        {
+            this.numStates = numStates;
+            this.numEvents = numEvents;
+        }
+
+        public delegate void delAction(); // unsure how to implement when passing into a method
+
 
         struct cell_FST
         {
             public int next_state;
-            public void (*action) (void);
+            public delegate void actions();
+            public actions FSM_action;
+
         }
+
+        cell_FST[,] FST;
 
         //Methods
         void SetNextState(int i, int j, int next_state)
         {
-            FST[i, j].next_state = this.next_state;
+            FST[i, j].next_state = next_state;
         }
-        void SetActions(int i, int j, int action)
+        void SetActions(int i, int j, delAction action)
         {
-            FST[i, j].action = this.action;
+            //delAction d1 = new delAction(action);
+            FST[i, j].FSM_action = action;
         }
 
-        int GetNextState(int i, int j)
+         public int GetNextState(int i, int j)
         {
             return FST[i, j].next_state;
         }
-        int GetActions(int i, int j)
+         public int GetActions(int i, int j)
         {
-            return 0;
+            return FST[i, j].Action();
         }
 
         //Main function
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Input Number of States:");
+            int numStates = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input Number of Events:");
+            int numEvents = Convert.ToInt32(Console.ReadLine());
+
+            var FST = new FiniteStateTable(numStates, numEvents);
         }
     }
 }
