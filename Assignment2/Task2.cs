0fde335b1db1ca9f5f11bb1fba5bcdc6380785b6
ljen819 <68795621+ljen819@users.noreply.github.com>
2021-06-02 17:37:40 +1200

    Update Task2.cs

--- Assignment2/Task2.cs
+++ Assignment2/Task2.cs
@@ -1,5 +1,6 @@
 /* Assignment 2: Task 2 - This task */
 
+// System set-up
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +14,14 @@ namespace Task2
 	{
 		public class FiniteStateMachine
 		{
-			interface IAction { } // Looking at Mario implementation
+			public enum States { S0, S1, S2, } // Set-up different states
+			public States State { get; set; } // Implement getter and setter for states
 
-			public enum States { S0, S1, S2, }
-			public States State { get; set; }
+			public enum Events { a, b, c } // Set-up different keyboard events
 
-			public enum Events { a, b, c }
+			public bool X_flag = false; // flags to ensure all actions occur for state transition
 
-			// flags to ensure both actions for state transition
-			public bool X_flag = false;
-
+			// Create delegate for FSM action implementation
 			public delegate void delActions();
 			private delActions[,] FSM;
 
@@ -38,100 +37,104 @@ namespace Task2
                 {this.W,        actions_XZ,         this.do_nothing},     //S1
                 {this.W,        this.do_nothing,    actions_XY} };        //S2
 			}
-			public void Process(Events Event)
+			
+			public void Process(Events Event) // Method that invokes the finite state machine
 			{
 				this.FSM[(int)this.State, (int)Event].Invoke();
 			}
-
-			public void W()
+	
+			public void W() // Action W Method
 			{
-				this.State = States.S0;
+				this.State = States.S0; // Change state to S0
 				Console.WriteLine("Action W");
 			}
-			public void X()
+			public void X() // Action X Method
 			{
-				X_flag = true;
+				X_flag = true; // set flag to true
 				Console.WriteLine("Action X");
 			}
-			public void Y()
+			public void Y() // Action Y Method
 			{
-				if (X_flag)
+				if (X_flag) // if Action X has occurred
 				{
-					this.State = States.S1;
+					this.State = States.S1; // Change state to S1
 					Console.WriteLine("Action Y");
-					X_flag = false;
+					X_flag = false; // reset Action X flag
 				}
 			}
-			public void Z()
+			public void Z() // Action Y Method
 			{
-				if (X_flag)
+				if (X_flag) // if Action X has occurred
 				{
-					this.State = States.S2;
+					this.State = States.S2; // Change state to S2
 					Console.WriteLine("Action Z");
-					X_flag = false;
+					X_flag = false; // reset Action X flag
 				}
 			}
-			private void do_nothing() { return; }
+			private void do_nothing() { return; } // method that returns nothing - for events that don't trigger transitions
 
-			public static String GetTimestamp(DateTime value)
+			public static String GetTimestamp(DateTime value) // method that gets time stamp 
 			{
 				string timeStamp = value.ToString("yyyyMMddHHmmssffff");
-				return timeStamp.Substring(4, 2) + "/" + timeStamp.Substring(6, 2) + " " + timeStamp.Substring(8, 2) + ":" + timeStamp.Substring(10, 2) + ":" + timeStamp.Substring(12, 2) + "	";
+				return timeStamp.Substring(4, 2) + "/" + timeStamp.Substring(6, 2) + " " + timeStamp.Substring(8, 2) + ":" + timeStamp.Substring(10, 2) + ":" + timeStamp.Substring(12, 2) + " ";
 			}
 
-			public static void Main(string[] args)
+			public static void Main(string[] args) // Main Function
 			{
 				
 				var FSM = new FiniteStateMachine(); // Creates instance of FiniteStateMachine()
 
+				// Ask user for file name input for .txt
 				Console.WriteLine("Please input a fully qualified text file name: ");
 				string textInput = Console.ReadLine();
 
+				// while loop that ensures user inputs .txt file is created
 				while(textInput.Substring(textInput.Length - 4) != ".txt")
                 {
 					Console.WriteLine("\nError: Re-enter text file name:");
 					textInput = Console.ReadLine();
 				}
 
-				var path = @textInput;
+				var path = @textInput; // sets the file path to input
 
 				ConsoleKeyInfo cki;
 
+				// Console interface with user
 				Console.WriteLine("\nBeginning in state " + FSM.State);
 				Console.WriteLine("Press any key to start:\n");
 				Console.WriteLine("Press 'q' to quit appllication\n");
 
-				File.WriteAllText(path, "Loggging Data:\n");
+				File.WriteAllText(path, "Loggging Data:\n"); // being logging data
 
 				do
 				{
 					cki = Console.ReadKey(true);
 
-					if(cki.Key == ConsoleKey.A)
+					if(cki.Key == ConsoleKey.A) // if user inputs 'a'
                     {
 						string timeStamp = GetTimestamp(DateTime.Now);
 						File.AppendAllText(path, timeStamp + " 'a' key pressed\n");
 
-						FSM.Process(FiniteStateMachine.Events.a);
+						FSM.Process(FiniteStateMachine.Events.a); // Process method ran for event a for FSM
 					}
 					else if (cki.Key == ConsoleKey.B)
 					{
 						string timeStamp = GetTimestamp(DateTime.Now);
 						File.AppendAllText(path, timeStamp + " 'b' key pressed\n");
 
-						FSM.Process(FiniteStateMachine.Events.b);
+						FSM.Process(FiniteStateMachine.Events.b); // Process method ran for event b for FSM
 					}
 					else if (cki.Key == ConsoleKey.C)
 					{
 						string timeStamp = GetTimestamp(DateTime.Now);
 						File.AppendAllText(path, timeStamp + " 'c' key pressed\n");
 
-						FSM.Process(FiniteStateMachine.Events.c);
+						FSM.Process(FiniteStateMachine.Events.c); // Process method ran for event c for FSM
 					}
 
 					if (cki.Key != ConsoleKey.Q)
 					{
-						Console.WriteLine("Now in state " + FSM.State + "\n");
+						Console.WriteLine("Now in state " + FSM.State + "\n"); // Write which state currently in
 					}
 					else
                     {
@@ -140,7 +143,7 @@ namespace Task2
 						Console.WriteLine("Now quitting application...");
                     }
 
-				} while (cki.Key != ConsoleKey.Q);
+				} while (cki.Key != ConsoleKey.Q); // continue loop until user presses 'q'
 
 			}
 		}
