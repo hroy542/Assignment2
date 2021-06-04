846c44d0e582415e93b969b3ab119eccb37155d5
ljen819 <68795621+ljen819@users.noreply.github.com>
2021-06-04 12:44:50 +1200

    Update Task2.cs

--- Assignment2/Task2.cs
+++ Assignment2/Task2.cs
@@ -1,4 +1,11 @@
-/* Assignment 2: Task 2 - This task */
+/* Assignment 2: Task 2 - This task creates a specified finite state machine and develops a console appliaction that interfaces
+ with the user through the keyboard. State transitions and actions are triggered using 'a', 'b', or 'c' and the application is 
+ quit using 'q'. A user inputted text file containing the triggered events and actions with timestamps is also created
+
+ Authors: 
+ Leighton Jensen (ljen819)
+ Hritom Roy (hroy542)
+ */
 
 // System set-up
 using System;
@@ -8,11 +15,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace Task2
+namespace task2
 {
 	public class Program
 	{
-		public class FiniteStateMachine
+		public class FiniteStateMachine // Class for task 2 finite state machine
 		{
 			public enum States { S0, S1, S2, } // Set-up different states
 			public States State { get; set; } // Implement getter and setter for states
@@ -25,7 +32,7 @@ namespace Task2
 			public delegate void delActions();
 			private delActions[,] FSM;
 
-			public FiniteStateMachine() // Maybe use finite state table (task 1)
+			public FiniteStateMachine() // contructor that creates table
 			{
 				delActions actions_XY, actions_XZ;
 				actions_XY = (delActions)X + (delActions)Y; // Perform actions X and Y 
@@ -37,12 +44,12 @@ namespace Task2
                 {this.W,        actions_XZ,         this.do_nothing},     //S1
                 {this.W,        this.do_nothing,    actions_XY} };        //S2
 			}
-			
+
 			public void Process(Events Event) // Method that invokes the finite state machine
 			{
 				this.FSM[(int)this.State, (int)Event].Invoke();
 			}
-	
+
 			public void W() // Action W Method
 			{
 				this.State = States.S0; // Change state to S0
@@ -73,79 +80,85 @@ namespace Task2
 			}
 			private void do_nothing() { return; } // method that returns nothing - for events that don't trigger transitions
 
-			public static String GetTimestamp(DateTime value) // method that gets time stamp 
+		}
+
+		public static String GetTimestamp(DateTime value) // method that gets time stamp 
+		{
+			string timeStamp = value.ToString("yyyyMMddHHmmssffff");
+			// returns time stamp string in DD/MM hh:mm:ss form
+			return timeStamp.Substring(6, 2) + "/" + timeStamp.Substring(4, 2) + " " + timeStamp.Substring(8, 2) + ":" + timeStamp.Substring(10, 2) + ":" + timeStamp.Substring(12, 2) + " ";
+		}
+
+		public static void Main(string[] args) // Main Function
+		{
+
+			var FSM = new FiniteStateMachine(); // Creates instance of FiniteStateMachine()
+
+			var currentState = FSM.State; // get the current state
+
+			// Ask user for file name input for .txt
+			Console.WriteLine("Please input a fully qualified text file name: ");
+			string textInput = Console.ReadLine();
+
+			// while loop that ensures user inputs .txt file is created
+			while (textInput.Substring(textInput.Length - 4) != ".txt")
 			{
-				string timeStamp = value.ToString("yyyyMMddHHmmssffff");
-				return timeStamp.Substring(4, 2) + "/" + timeStamp.Substring(6, 2) + " " + timeStamp.Substring(8, 2) + ":" + timeStamp.Substring(10, 2) + ":" + timeStamp.Substring(12, 2) + " ";
+				Console.WriteLine("\nError: Re-enter text file name:");
+				textInput = Console.ReadLine();
 			}
 
-			public static void Main(string[] args) // Main Function
+			var path = @textInput; // sets the file path to input
+
+			ConsoleKeyInfo cki;
+
+			// Console interface with user
+			Console.WriteLine("\nBeginning in state " + FSM.State);
+			Console.WriteLine("Press any key to start:\n");
+			Console.WriteLine("Press 'q' to quit appllication\n");
+
+			File.WriteAllText(path, "Loggging Data:\n"); // being logging data
+
+			do
 			{
-				
-				var FSM = new FiniteStateMachine(); // Creates instance of FiniteStateMachine()
+				currentState = FSM.State; // update current state
 
-				// Ask user for file name input for .txt
-				Console.WriteLine("Please input a fully qualified text file name: ");
-				string textInput = Console.ReadLine();
+				cki = Console.ReadKey(true);
 
-				// while loop that ensures user inputs .txt file is created
-				while(textInput.Substring(textInput.Length - 4) != ".txt")
-                {
-					Console.WriteLine("\nError: Re-enter text file name:");
-					textInput = Console.ReadLine();
+				if (cki.Key == ConsoleKey.A) // if user inputs 'a'
+				{
+					string timeStamp = GetTimestamp(DateTime.Now);
+					File.AppendAllText(path, timeStamp + " 'a' key pressed\n");
+
+					FSM.Process(FiniteStateMachine.Events.a); // Process method ran for event a for FSM
+				}
+				else if (cki.Key == ConsoleKey.B)
+				{
+					string timeStamp = GetTimestamp(DateTime.Now);
+					File.AppendAllText(path, timeStamp + " 'b' key pressed\n");
+
+					FSM.Process(FiniteStateMachine.Events.b); // Process method ran for event b for FSM
+				}
+				else if (cki.Key == ConsoleKey.C)
+				{
+					string timeStamp = GetTimestamp(DateTime.Now);
+					File.AppendAllText(path, timeStamp + " 'c' key pressed\n");
+
+					FSM.Process(FiniteStateMachine.Events.c); // Process method ran for event c for FSM
 				}
 
-				var path = @textInput; // sets the file path to input
-
-				ConsoleKeyInfo cki;
-
-				// Console interface with user
-				Console.WriteLine("\nBeginning in state " + FSM.State);
-				Console.WriteLine("Press any key to start:\n");
-				Console.WriteLine("Press 'q' to quit appllication\n");
-
-				File.WriteAllText(path, "Loggging Data:\n"); // being logging data
-
-				do
+				if ((cki.Key != ConsoleKey.Q) && (currentState != FSM.State)) // if not quitting and a state transition has occurred
 				{
-					cki = Console.ReadKey(true);
+					Console.WriteLine("Now in state " + FSM.State + "\n"); // Write which state currently in
+				}
+				else if (cki.Key == ConsoleKey.Q) // Console interface for quitting application
+				{
+					string timeStamp = GetTimestamp(DateTime.Now);
+					File.AppendAllText(path, timeStamp + " 'q' key pressed - Closing Application\n");
+					Console.WriteLine("Now quitting application...");
+				}
 
-					if(cki.Key == ConsoleKey.A) // if user inputs 'a'
-                    {
-						string timeStamp = GetTimestamp(DateTime.Now);
-						File.AppendAllText(path, timeStamp + " 'a' key pressed\n");
+			} while (cki.Key != ConsoleKey.Q); // continue loop until user presses 'q'
 
-						FSM.Process(FiniteStateMachine.Events.a); // Process method ran for event a for FSM
-					}
-					else if (cki.Key == ConsoleKey.B)
-					{
-						string timeStamp = GetTimestamp(DateTime.Now);
-						File.AppendAllText(path, timeStamp + " 'b' key pressed\n");
-
-						FSM.Process(FiniteStateMachine.Events.b); // Process method ran for event b for FSM
-					}
-					else if (cki.Key == ConsoleKey.C)
-					{
-						string timeStamp = GetTimestamp(DateTime.Now);
-						File.AppendAllText(path, timeStamp + " 'c' key pressed\n");
-
-						FSM.Process(FiniteStateMachine.Events.c); // Process method ran for event c for FSM
-					}
-
-					if (cki.Key != ConsoleKey.Q)
-					{
-						Console.WriteLine("Now in state " + FSM.State + "\n"); // Write which state currently in
-					}
-					else
-                    {
-						string timeStamp = GetTimestamp(DateTime.Now);
-						File.AppendAllText(path, timeStamp + " 'q' key pressed - Closing Application\n");
-						Console.WriteLine("Now quitting application...");
-                    }
-
-				} while (cki.Key != ConsoleKey.Q); // continue loop until user presses 'q'
-
-			}
 		}
 	}
 }
