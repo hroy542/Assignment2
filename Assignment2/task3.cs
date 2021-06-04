5ec90057c65a471816b3d5d5ddebd01925ff4406
ljen819 <68795621+ljen819@users.noreply.github.com>
2021-06-04 15:04:39 +1200

    Update task3.cs

--- Assignment2/task3.cs
+++ Assignment2/task3.cs
@@ -1,3 +1,11 @@
+/* Assignment 2: Task 3 - This task implements 2 concurrent, dependent, synchonous finite state machines. The
+ code extended from task 2 whereby another FSM is implemented using multi-threading. Similar to task 2 a console
+ application is created which interfaces to the user through the keyboard. A file is similarly created which again
+ prints all the trigger events and actions.
+
+ Authors: Leighton Jensen (ljen819), Hritom Roy (hroy542)
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,11 +13,11 @@ using System.Text;
 using System.Threading;
 using System.IO;
 
-namespace Task3
+namespace Task3 
 {
 	public class Program
 	{
-		public class FiniteStateMachine1
+		public class FiniteStateMachine1 // FSM for task 2
 		{
 			public enum States { S0, S1, S2, } // Set-up states for task 2 
 			public States State { get; set; } // Implement getter and setter for states
@@ -69,20 +77,14 @@ namespace Task3
 				}
 			}
 			private void do_nothing() { return; } // method that returns nothing - for events that don't trigger transitions
-
-			public static String GetTimestamp(DateTime value) // method that gets time stamp 
-			{
-				string timeStamp = value.ToString("yyyyMMddHHmmssffff");
-				return timeStamp.Substring(4, 2) + "/" + timeStamp.Substring(6, 2) + " " + timeStamp.Substring(8, 2) + ":" + timeStamp.Substring(10, 2) + ":" + timeStamp.Substring(12, 2) + " ";
-			}
 		}
 
-		public class FiniteStateMachine2
+		public class FiniteStateMachine2 // fsm for task 3
 		{
 			public enum States { SB, SA } // Set-up states for task 3
 			public States State { get; set; } // getter and setter for task 3 states
 
-
+			// flags for actions J and K set up
 			public bool J_flag = false;
 			public bool K_flag = false;
 
@@ -114,112 +116,180 @@ namespace Task3
 
 		}
 
-		public static String GetTimestamp(DateTime value) // method that gets time stamp 
+		public static String GetTimestamp(DateTime value) // method that gets time stamp in DD/MM hh:mm:ss form
 		{
 			string timeStamp = value.ToString("yyyyMMddHHmmssffff");
-			return timeStamp.Substring(4, 2) + "/" + timeStamp.Substring(6, 2) + " " + timeStamp.Substring(8, 2) + ":" + timeStamp.Substring(10, 2) + ":" + timeStamp.Substring(12, 2) + " ";
+			return timeStamp.Substring(6, 2) + "/" + timeStamp.Substring(4, 2) + " " + timeStamp.Substring(8, 2) + ":" + timeStamp.Substring(10, 2) + ":" + timeStamp.Substring(12, 2) + " ";
 		}
 
 		public static void Main(string[] args) // Entry point to program - begin main thread
 		{
-			// THREADING HERE
+			// create new instances for both finite state machines
 			var fsm1 = new FiniteStateMachine1();
 			var fsm2 = new FiniteStateMachine2();
 
+			// current state variables
 			var currentState1 = fsm1.State;
 			var currentState2 = fsm2.State;
 
+			// flags for task 2
+			bool S0_flag = true;
+			bool S1_flag = false;
+			bool S2_flag = false;
+
+			// flags for task 3
 			bool SB_flag = true;
 			bool SA_flag = false;
 
-			// Ask user for file name input for .txt
-			Console.WriteLine("Please input a fully qualified text file name: ");
-			string textInput = Console.ReadLine();
+			// strings for data logging
+			string timeStamp;
+			string allText = "";
 
-			// while loop that ensures user inputs .txt file is created
-			while (textInput.Substring(textInput.Length - 4) != ".txt")
-			{
-				Console.WriteLine("\nError: Re-enter text file name:");
-				textInput = Console.ReadLine();
-			}
-
-			var path = @textInput; // sets the file path to input
-
-			ConsoleKeyInfo cki;
+			ConsoleKeyInfo cki; // for reading key inputs
 
 			// Console interface with user
 			Console.WriteLine("\nTask 2 FSM beginning in state " + fsm1.State);
 			Console.WriteLine("Task 3 FSM beginning in state " + fsm2.State);
-			Console.WriteLine("Press any key to start:\n");
+			Console.WriteLine("\nPress 'a' to start:\n");
 			Console.WriteLine("Press 'q' to quit appllication\n");
-
-			File.WriteAllText(path, "Loggging Data:\n"); // being logging data
 
 			do
 			{
-				cki = Console.ReadKey(true);
+				cki = Console.ReadKey(true); // read key input
 
+				// Create new threads for actions J, K, and L
 				Thread thrJ = new Thread(fsm2.J);
 				Thread thrK = new Thread(fsm2.K);
 				Thread thrL = new Thread(fsm2.L);
 				Thread thrTransition = new Thread(fsm2.transition);
 
+				// Update the current state
 				currentState1 = fsm1.State;
 				currentState2 = fsm2.State;
 
 				if (cki.Key == ConsoleKey.A) // if user inputs 'a'
 				{
-					string timeStamp = GetTimestamp(DateTime.Now);
-					File.AppendAllText(path, timeStamp + " 'a' key pressed\n");
-
+					timeStamp = GetTimestamp(DateTime.Now);
+					allText = string.Concat(allText, timeStamp + "	'a' key pressed\n");
+					
 					fsm1.Process(FiniteStateMachine1.Events.a); // Process method ran for event a for FSM
 
-					if (SA_flag)
+					// change flags to indicate current state and insert into logging string
+					if (S0_flag) 
+					{ 
+						S1_flag = true; 
+						S0_flag = false;
+						S2_flag = false;
+						allText = string.Concat(allText, timeStamp + "	Action X\n");
+						allText = string.Concat(allText, timeStamp + "	Action Y\n");
+					}
+					else if (S1_flag || S2_flag) // if in S1 or S2
+					{ 
+						S0_flag = true; 
+						S1_flag = false; 
+						S2_flag = false;
+						allText = string.Concat(allText, timeStamp + "	Action W\n");
+					}
+
+					if (SA_flag) // if in SA state, transition to SB
 					{
 						thrTransition.Start();
-						Thread.Sleep(1);
+						Thread.Sleep(1); // 1ms pause to ensure order maintained
 
+						// invert flags
 						SA_flag = false;
 						SB_flag = true;
 					}
 
 				}
-				else if (cki.Key == ConsoleKey.B)
+				else if (cki.Key == ConsoleKey.B) // if user inputs 'b'
 				{
-					string timeStamp = GetTimestamp(DateTime.Now);
-					File.AppendAllText(path, timeStamp + " 'b' key pressed\n");
+					timeStamp = GetTimestamp(DateTime.Now);
 
-					fsm1.Process(FiniteStateMachine1.Events.b); // Process method ran for event b for FSM
-				}
-				else if (cki.Key == ConsoleKey.C)
-				{
-					string timeStamp = GetTimestamp(DateTime.Now);
-					File.AppendAllText(path, timeStamp + " 'c' key pressed\n");
-
-					fsm1.Process(FiniteStateMachine1.Events.c); // Process method ran for event c for FSM
-				}
-
-				if (cki.Key != ConsoleKey.Q)
-				{
-					if (currentState1 != fsm1.State) { Console.WriteLine("Task 2 FSM now in state " + fsm1.State + "\n"); } // Write which state currently in
-
-					if ((fsm1.State == FiniteStateMachine1.States.S1) && (SB_flag))
+					if (S1_flag) // if current state is S1, trigger process
 					{
-						thrJ.Start();
-						thrK.Start();
-						thrL.Start();
-						Thread.Sleep(1);
+						fsm1.Process(FiniteStateMachine1.Events.b); // Process method ran for event b for FSM
 
+						// change state flags
+						S2_flag = true;
+						S0_flag = false;
+						S1_flag = false;
+
+						// Add actions and events to logging string
+						allText = string.Concat(allText, timeStamp + "	'b' key pressed\n");
+						allText = string.Concat(allText, timeStamp + "	Action X\n");
+						allText = string.Concat(allText, timeStamp + "	Action Z\n");
+					}
+
+				}
+				else if (cki.Key == ConsoleKey.C) // if user inputs 'c'
+				{
+					timeStamp = GetTimestamp(DateTime.Now);
+
+					if (S2_flag) // if current state is S2, trigger process
+					{
+						fsm1.Process(FiniteStateMachine1.Events.c); // Process method ran for event c for FSM
+
+						// change state flags
+						S1_flag = true;
+						S0_flag = false;
+						S2_flag = false;
+
+						// Add actions and events to logging string
+						allText = string.Concat(allText, timeStamp + "	'c' key pressed\n");
+						allText = string.Concat(allText, timeStamp + "	Action X\n");
+						allText = string.Concat(allText, timeStamp + "	Action Y\n");
+					}
+				}
+
+				if (cki.Key != ConsoleKey.Q) // if application is running
+				{
+					if (currentState1 != fsm1.State) { Console.WriteLine("Task 2 FSM now in state " + fsm1.State + "\n"); } // Display task 2 state
+
+					if ((fsm1.State == FiniteStateMachine1.States.S1) && (SB_flag)) // if we are in state S1 for task 2 and SB
+					{
+						thrJ.Start(); // start action J thread
+						timeStamp = GetTimestamp(DateTime.Now);
+						allText = string.Concat(allText, timeStamp + "	Action J\n");
+
+						thrK.Start(); // start action K thread
+						timeStamp = GetTimestamp(DateTime.Now);
+						allText = string.Concat(allText, timeStamp + "	Action K\n");
+
+						thrL.Start(); // start action L thread
+						timeStamp = GetTimestamp(DateTime.Now);
+						allText = string.Concat(allText, timeStamp + "	Action L\n");
+
+						Thread.Sleep(1); // 1ms pause to ensure order maintained
+
+						// invert flags
 						SB_flag = false;
 						SA_flag = true;
 					}
 
-					if (currentState2 != fsm2.State) { Console.WriteLine("Task 3 FSM now in state " + fsm2.State + "\n"); }
+					if (currentState2 != fsm2.State) { Console.WriteLine("Task 3 FSM now in state " + fsm2.State + "\n"); } // Display task 3 state
 				}
-				else
+				else // if 'q' has been pressed - closing application
 				{
-					string timeStamp = GetTimestamp(DateTime.Now);
-					File.AppendAllText(path, timeStamp + " 'q' key pressed - Closing Application\n");
+					timeStamp = GetTimestamp(DateTime.Now);
+					allText = string.Concat(allText, timeStamp + "	'q' key pressed - Closing Application\n");
+
+					// Ask user for file name input for .txt
+					Console.WriteLine("Please input a fully qualified text file name: ");
+					string textInput = Console.ReadLine();
+
+					// while loop that ensures user inputs .txt file 
+					while (textInput.Substring(textInput.Length - 4) != ".txt")
+					{
+						Console.WriteLine("\nError: Re-enter text file name:");
+						textInput = Console.ReadLine();
+					}
+
+					var path = @textInput; // sets the file path to input
+
+					File.WriteAllText(path, allText); // writes log to text file
+
+					Console.WriteLine("\nSuccessful loggin to file");
 					Console.WriteLine("Now quitting application...");
 				}
 
